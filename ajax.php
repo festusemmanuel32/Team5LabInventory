44fe8e4ac90cbdb758e3326fb00b2a3ff3466c58
Lydia Kinyari <lydiakemmy@gmail.com>
2015-11-14 16:27:08 +0000

    modified the ajax file

--- ajax.php
+++ ajax.php
@@ -4,36 +4,21 @@ $cmd=$_REQUEST['cmd'];
 switch($cmd)
 {
 	case 1:
-		include("addReading.php");
-		$obj=new addReading();
-		$meterID=$_GET['meter'];
-	        $meterValue=$_GET['reading'];
-	        //echo $_GET['meter']; 
-		$row=$obj->add($meterID,$meterValue);
+		include("equipment.php");
+		$obj=new equipment();
+		$id=$_GET['id'];
+	    $name=$_GET['name'];
+	    $price=$_GET['price'];
+	    $supplier=$_GET['supplier'];
+	    $location=$_GET['location'];
+	    $status=$_GET['status'];
+	     echo $_GET['id']; 
+		if($row=$obj->add($id,$name,$price,$supplier,$location,$status))
                 echo '{"result":"1"}';
-		//return a JSON string to browser when request comes to get description
-				
-		break;
-	case 2:
-		include("addReading.php");
-		$obj=new addReading();
+                else
+                echo "mysql_error";
 
-		
-		if($row=$obj->getReadings()){
-			$row=$obj->fetch();		
-			echo '{"result":1,"message":[';
-			while ( $row ){
-				echo json_encode($row);
-				$row = $obj->fetch (); 
-				if ($row){
-					echo ',';
-				}
-			}
-		echo ']}';
-
-		}else{
-			echo '{"result":0,"message": "Failed"}';
-		}
+		//return a JSON string to browser when request comes to get description				
 		break;	
 	default:
 }
