4ca55a6b006f8257ad8b20a7dca85f19fcdfea23
festus emmanuel <festus.jartu@ashesi.edu.gh>
2015-11-18 12:52:52 +0000

    it can now take care ofthe letters

--- adb.php
+++ adb.php
@@ -1,142 +1,70 @@
-<?php
+var randomLoc = Math.floor(Math.random() * 5);
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location1 + 2;
+var guess;
+var hits = 0;
+var guesses = 0;
+var isSunk = false;
 
-/**
- * author: 
- * date:
- * description: A root class for all manage classes. This class communicates with DB
- */
+function game(playhere,buttonid)
+{
+    guess = playhere;    
+    guesses = guesses + 1;
+    //alert("You Clicked 0");
+    //document.getElementById("putmedown").innerHTML="You've Clicked" + " " + playhere;
+    if (guesses == 5)
+    {
+        //alert("You have run out of guesses. Game over. You Lost.");
+        document.getElementById("letmedie").innerHTML="You have run out of guesses. Game over. You Lost.";
 
-define("DB_HOST", 'localhost');
-define("DB_NAME", 'readings');
-define("DB_PORT", 3307);
-define("DB_USER",'root');
-define("DB_PWORD",'');
+        endGane();
+    }
+    else
+    {
+        document.getElementById(buttonid).disabled = true;
+        playgame(buttonid);
+    }
+}
+    
+function playgame(buttonid){
+    if (guess == location1 || guess == location2 || guess == location3) {
+            //alert("HIT!");
+            document.getElementById(buttonid).innerHTML="HIT!";
 
+            hits = hits + 1;
+            if (hits == 3) {
+                isSunk = true;
+                //alert("You sank my battleship!");
+                endGane();
+                document.getElementById("letmedie").innerHTML="You sank my battleship!";
+            }
+        } else {
+            //alert("MISS");
+            document.getElementById(buttonid).innerHTML="MISS";
 
+        }
+}
+function restartg()
+window.location="battleship.html";
+{
 
-
-
-
-define("LOG_LEVEL_SEC",0);
-define("LOG_LEVEL_DB_FAIL",0);
-
-define("PAGE_SIZE",10);
-
-function log_msg($level, $er_code, $msg, $mysql_msg){
-    return 0;
 }
 
-class adb {
-
-    /**error description*/
-    var $str_error;
-    /*error code*/
-    var $error;
-    /*db connection link*/
-    var $link;
-    /* Every error log has a 4 digit code. The first two digits(prefix) tells you which class logged the error*/
-    var $er_code_prefix;
-    /* query result resource*/
-    var $result;
-///+
-    function adb() {
-       
-        $this->er_code_prefix=1000;
-        $this->link=false;
-        $this->result = false;
-    }
-
-    /**
-     * logs error into database using functions defined in log.php
-     */
-    function log_error($level, $code, $msg, $mysql_msg = "NONE") {
-        $er_code = $this->er_code_prefix + $code;
-        //call to a predefined function 
-        $log_id = log_msg($level, $er_code, $msg, $mysql_msg);
-        //if log id is false return 0;
-        if (!$log_id) {
-            return 0;
-        }
-
-        //display this code to user
-        $this->error="$er_code-$log_id";
-        return $log_id;
-    }
-
-    /**
-    * creates connection to database
-    */
-    function connect() {
-
-        if($this->link)
-        {
-            return true;
-        }
-        //try to connect to db
-        $this->link = mysql_connect(DB_HOST , DB_USER, DB_PWORD);
-        
-        if (!$this->link) {
-            //if connection fail log error and set $str_error
-            //echo "not connected"; //debug line
-            echo "not connect2";
-            $this->log_error(LOG_LEVEL_DB_FAIL,1, "connection failed  in db:connect()", mysql_error());
-            return false;
-        }
-        //echo "connected";
-        if (!mysql_select_db(DB_NAME)) {
-            echo "erro connecting";
-            $log_id = $this->log_error(LOG_LEVEL_DB_FAIL,2, "select db failed   in db:connect()", mysql_error($this->link));
-            return false;
-        }
-
-
-        return true;
-    }
- 
-        
-    /**
-    *returns a row from a data set
-    */
-    function fetch() {
-        return mysql_fetch_assoc($this->result);
-    }
-
-    /**
-    * connect to db and run a query 
-    */
-    function query($str_sql) {
-        
-        if (!$this->connect()) {        
-            return false;
-        }
-        
-        $this->result = mysql_query($str_sql,$this->link);
-        if (!$this->result) {
-            $this->log_error(LOG_LEVEL_DB_FAIL, 4, "query failed", mysql_error($this->link));
-            return false;
-        }
-
-        return true;
-    }
-    
-    /**
-    * returns number of rows in current dataset
-    */
-    function get_num_rows() {
-        return mysql_num_rows($this->result);
-    }
-    /**
-    *returns last auto generated id 
-    */
-    function get_insert_id() {
-        return mysql_insert_id($this->link);
-    }
-    
+function endGane(){
+    document.getElementById("locone").disabled = true;
+    document.getElementById("lacatwo").disabled = true;
+    document.getElementById("lacathree").disabled = true;
+    document.getElementById("lacafour").disabled = true;
+    document.getElementById("lacafive").disabled = true;
+    document.getElementById("lacasix").disabled = true;
+    document.getElementById("lacasev").disabled = true;    
+    showstat();
 }
 
-?>
-
-
-
-
-
+function showstat(){
+    var stats = "You took " + guesses + " guesses to sink the battleship, " +
+            "which means your shooting accuracy was " + (3/guesses);
+            document.getElementById("putmedown").innerHTML=stats;
+    //alert(stats);
+}
