b733ac3ba710ed89100933bbf7e6bf8ee5549a23
festus emmanuel <festus.jartu@ashesi.edu.gh>
2015-11-20 02:05:22 +0000

    commiting changes made to the ajax file to hadle searching a supplier

--- supplyAjax.php
+++ supplyAjax.php
@@ -13,8 +13,27 @@ switch($cmd)
 	$contact=$_GET['cont'];
 	$row=$obj->addSupplier($supplierID, $contact, $supplyQuant, $supplierName);
 		//return a JSON string to browser when request comes to get description
-
 break;
+	case 2:
+	include("Supply.php");
+	$obj=new Supply();
+	if($row=$obj->get_supply()){		
+		echo '{"result":1,"message":[';
+		while ($row){
+			echo json_encode($row);
+			$row=$obj->fetch();
+			if ($row) {
+				echo ",";
+			}
+		}
+		echo "]}";
+	}
+	else{
+		echo '{"result":0,"message": "reading not removed."}';
+	}
+	break;	
+
+
 
 }
 ?>
