721d6e27073c309ffbee1fdfacf44a19025cc8d5
Lydia Kinyari <lydiakemmy@gmail.com>
2015-11-23 00:57:34 +0000

    updated the add functional requirement

--- equipment.php
+++ equipment.php
@@ -6,5 +6,19 @@ class equipment extends adb{
 		$str_query="insert into eqpt set id = '$id', name = '$name', price = $price, supplier = '$supplier', location = '$location', status = '$status'";
 		return $this->query($str_query);
 	}
+
+	function view(){
+		$str_query="select * from eqpt";
+		$this->query($str_query);
+		return $this->fetch();
+	}
+	function update($id,$name,$price,$supplier,$location,$status){
+		$str_query="update eqpt set name = '$name', price = $price, supplier = '$supplier', location = '$location', status = '$status' where  id = '$id'";
+		return $this->query($str_query);
+	}
+	function delete($id){
+		$str_query="delete from eqpt where id='$id'";
+		return $this->query($str_query);
+	}
 }
 ?>
